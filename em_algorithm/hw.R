432f661b5e75866f1356f52d29dea442290eb894
jtleek <jtleek@gmail.com>
2015-12-07 13:03:05 -0500

    updated with hw

---
+++ em_algorithm/hw.R
@@ -0,0 +1,7 @@
+path <- .path_to_file("advdatasci", "em_algorithm", "hw.Rmd")
+
+tf <- tempfile()
+dir.create(tf)
+htmlFile <- file.path(tf, "index.html")
+suppressWarnings(rmarkdown::render(path, output_format = "html_document", output_file = htmlFile))
+rstudioapi::viewer(htmlFile)
--- em_algorithm/initLesson.R
+++ em_algorithm/initLesson.R
@@ -1,3 +1,9 @@
-# Put initialization code in this file. The variables you create
-# here will show up in the user's workspace when he or she begins
-# the lesson.
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+.path_to_file <<- function(course, lesson, file_name){
+  file.path(path.package('swirl'), 'Courses', course,
+            lesson, file_name)
+}
