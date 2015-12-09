7082aa864f1fb0ab10d48bbd787684d71548a82c
jtleek <jtleek@gmail.com>
2015-12-09 07:03:04 -0500

    updated swirl

--- em_algorithm/hw.R
+++ em_algorithm/hw.R
@@ -1,4 +1,9 @@
-htmlfile <- .path_to_file("advdatasci-swirl", "em_algorithm/html", "hw.html")
+filename ="hw.html"
+swirlfile <- .path_to_file("advdatasci-swirl", "em_algorithm/html", filename)
+tf <- tempfile()
+dir.create(tf)
+htmlfile = file.path(tf,filename)
+file.copy(swirlfile,htmlfile)
 if(rstudioapi::isAvailable()){
   rstudioapi::viewer(htmlfile)
 }else{
